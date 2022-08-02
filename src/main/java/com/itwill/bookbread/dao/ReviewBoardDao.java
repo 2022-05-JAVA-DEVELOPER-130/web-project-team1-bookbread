b9bc80c5254bb2d866adda06ec16f130acdbdcbb
journeyjw <kjw0258@naver.com>
2022-08-02 20:36:50 +0900

    ..

--- src/main/java/com/itwill/bookbread/dao/ReviewBoardDao.java
+++ src/main/java/com/itwill/bookbread/dao/ReviewBoardDao.java
@@ -1,6 +1,9 @@
 package com.itwill.bookbread.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -8,6 +11,9 @@ import javax.sql.DataSource;
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 import com.itwill.bookbread.dto.ReviewBoard;
+import com.itwill.bookbread.sql.ReviewBoardSQL;
+import com.itwill.user.User;
+import com.itwill.user.UserSQL;
 
 public class ReviewBoardDao {
 	private DataSource dataSource;
@@ -28,17 +34,139 @@ public class ReviewBoardDao {
 	 */
 	public int create(ReviewBoard reviewBoard) throws Exception{
 		Connection con=null;
+		PreparedStatement pstmt=null;
+		int insertRowCount=0;
+		try {
+			con=dataSource.getConnection();
+			pstmt=con.prepareStatement(ReviewBoardSQL.REVIEW_INSERT);
+			pstmt.setString(1,reviewBoard.getR_title());
+			pstmt.setString(2, reviewBoard.getR_content());
+			pstmt.setInt(3, reviewBoard.getR_count());
+			pstmt.setString(4, reviewBoard.getUserId());
+			pstmt.setInt(5, reviewBoard.getP_no());
+		} finally {
+			insertRowCount=pstmt.executeUpdate();
+			if(pstmt!=null) {
+				pstmt.close();
+			}
+			if(con!=null) {
+				con.close();
+			}
+		}
+		return insertRowCount;
 	}
 	
+	/*
+	 * 리뷰수정
+	 */
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	public int update(ReviewBoard reviewBoard) throws Exception{
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int updateRowCount=0;
+		try {
+			con=dataSource.getConnection();
+			pstmt=con.prepareStatement(ReviewBoardSQL.REVIEW_UPDATE);
+			pstmt.setString(1,reviewBoard.getR_title());
+			pstmt.setString(2, reviewBoard.getR_content());
+			pstmt.setInt(3, reviewBoard.getR_no());
+		} finally {
+			updateRowCount=pstmt.executeUpdate();
+			if(pstmt!=null) {
+				pstmt.close();
+			}
+			if(con!=null) {
+				con.close();
+			}
+		}
+		return updateRowCount;
+	}
+	/*
+	 * 리뷰삭제
+	 */
+	public int remove(String userId) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int removeRowCount = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ReviewBoardSQL.REVIEW_DELETE);
+			pstmt.setString(1, userId);
+			removeRowCount = pstmt.executeUpdate();
+
+		} finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
+
+		}
+		return removeRowCount;
+	}
+	/*
+	 * userId가쓴 리뷰 보기
+	 */
+	public ReviewBoard findReview(String userId) throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ReviewBoard findReview=null;
+		try {
+			con = dataSource.getConnection();
+			pstmt=con.prepareStatement(ReviewBoardSQL.REVIEW_SELECET_BY_ID);
+			pstmt.setString(1, userId);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				findReview = new ReviewBoard(rs.getInt("r_no"),
+											 rs.getDate("r_date"),
+											 rs.getString("r_title"),
+											 rs.getString("r_content"),
+											 rs.getInt("r_count"),
+											 null,
+											 null);
+			}
+					
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
+		}
+		return findReview;
+	}
+	/*
+	 * 전체리뷰보기
+	 */
+	public ArrayList<ReviewBoard> findReviewList() throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<ReviewBoard> findReviewList = new ArrayList<ReviewBoard>();
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ReviewBoardSQL.REVIEW_SELECT_ALL);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				findReviewList.add(new ReviewBoard(rs.getInt("r_no"),
+											       rs.getDate("r_date"),
+											       rs.getString("r_title"),
+											       rs.getString("r_content"),
+											       rs.getInt("r_count"),
+											       null,
+											       null));
+			}
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
+		}
+		return findReviewList;
+	}
 	
 	
 	
--- src/main/java/com/itwill/bookbread/test/ReviewBoardDaoTestMain.java
+++ src/main/java/com/itwill/bookbread/test/ReviewBoardDaoTestMain.java
@@ -1,9 +1,17 @@
 package com.itwill.bookbread.test;
 
+import java.util.Date;
+
+import com.itwill.bookbread.dao.ReviewBoardDao;
+import com.itwill.bookbread.dto.Member;
+import com.itwill.bookbread.dto.Product;
+import com.itwill.bookbread.dto.ReviewBoard;
+
 public class ReviewBoardDaoTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		ReviewBoardDao reviewBoardDao=new ReviewBoardDao();
+		
 
 	}
 
