c8df2b0fbe6d621cf81938fcb1af8b999b7cdd5c
KimHeeCheul <kim11403@naver.com>
2022-08-02 20:05:39 +0900

    ab

--- src/main/java/com/itwill/bookbread/service/MemberService.java
+++ src/main/java/com/itwill/bookbread/service/MemberService.java
@@ -1,6 +1,45 @@
 package com.itwill.bookbread.service;
 
+import com.itwill.bookbread.dao.MemberDao;
 
 public class MemberService {
-
+	private MemberDao memberDao;
+	
+	public MemberService() throws Exception {
+		memberDao = new MemberDao();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
