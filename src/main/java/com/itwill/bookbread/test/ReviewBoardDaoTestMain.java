a598a2f328681d27436f9e7c6c60245291c34261
Juniverse <ggolre90@gmail.com>
2022-08-02 23:11:23 +0900

    reviewboardDao 수정

--- src/main/java/com/itwill/bookbread/test/ReviewBoardDaoTestMain.java
+++ src/main/java/com/itwill/bookbread/test/ReviewBoardDaoTestMain.java
@@ -11,8 +11,9 @@ public class ReviewBoardDaoTestMain {
 
 	public static void main(String[] args) throws Exception {
 		ReviewBoardDao reviewBoardDao=new ReviewBoardDao();
-		System.out.println(reviewBoardDao.create(new ReviewBoard(0,null,"책이름","책내용",1,new Member(),new Product())));
+		//System.out.println(reviewBoardDao.create(new ReviewBoard(0,null,"책이름","책내용",1,new Member(),new Product())));
 		
+		System.out.println(reviewBoardDao.findReviewList());
 
 	}
 
