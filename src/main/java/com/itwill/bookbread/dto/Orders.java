1fdccec71d083138755f89e0a3c6e8d19ff5baa2
ddoyoon <ddoyoon@localhost>
2022-08-02 11:24:59 +0900

    ..

--- src/main/java/com/itwill/bookbread/dto/Orders.java
+++ src/main/java/com/itwill/bookbread/dto/Orders.java
@@ -1,5 +1,81 @@
 package com.itwill.bookbread.dto;
 
-public class Orders {
+import java.util.Date;
 
+/*
+ * 이름      널?       유형            
+------- -------- ------------- 
+O_NO    NOT NULL NUMBER(10)    
+O_DESC           VARCHAR2(100) 
+O_DATE           DATE          
+O_PRICE          NUMBER(10)    
+USERID           VARCHAR2(100) 
+ */
+public class Orders {
+	private int o_no;
+	private String o_desc;
+	private Date o_date;
+	private int o_price;
+	private String userid;
+	 
+	public Orders() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Orders(int o_no, String o_desc, Date o_date, int o_price, String userid) {
+		super();
+		this.o_no = o_no;
+		this.o_desc = o_desc;
+		this.o_date = o_date;
+		this.o_price = o_price;
+		this.userid = userid;
+	}
+
+	public int getO_no() {
+		return o_no;
+	}
+
+	public void setO_no(int o_no) {
+		this.o_no = o_no;
+	}
+
+	public String getO_desc() {
+		return o_desc;
+	}
+
+	public void setO_desc(String o_desc) {
+		this.o_desc = o_desc;
+	}
+
+	public Date getO_date() {
+		return o_date;
+	}
+
+	public void setO_date(Date o_date) {
+		this.o_date = o_date;
+	}
+
+	public int getO_price() {
+		return o_price;
+	}
+
+	public void setO_price(int o_price) {
+		this.o_price = o_price;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	@Override
+	public String toString() {
+		return "Orders [o_no=" + o_no + ", o_desc=" + o_desc + ", o_date=" + o_date + ", o_price=" + o_price
+				+ ", userid=" + userid + "]";
+	}
+	
+	
 }
