b675ad5b428ea73546e4a147533c9a2babb1187e
KimHeeCheul <kim11403@naver.com>
2022-08-02 19:12:24 +0900

    ..

--- src/main/java/com/itwill/bookbread/test/MemberDaoTestMain.java
+++ src/main/java/com/itwill/bookbread/test/MemberDaoTestMain.java
@@ -1,10 +1,23 @@
 package com.itwill.bookbread.test;
 
+import com.itwill.bookbread.dao.MemberDao;
+
 public class MemberDaoTestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MemberDao memberDao = new MemberDao();
+		//System.out.println(memberDao.create(new Member("bbb","aaa","aaa","aaa","aaa","aaa","aaa","aaa")));
+		//System.out.println(memberDao.update(new Member("heechul", "null", "김희철", "010-6666-6666", "2022-11-11", "서울시 강서구", "kim@testmail.com", "동화")));
+		//System.out.println(memberDao.delete("bbb"));
+		//System.out.println(memberDao.findMember("aaa"));
+		//System.out.println(memberDao.findMember("ddoyoon"));
+		//System.out.println(memberDao.findMember("bbb"));
+		//System.out.println(memberDao.findMemeber());
+		//System.out.println(memberDao.findId("김희철", "010-6666-6666"));
+		//System.out.println(memberDao.findPassword("heechul", "heechul@testmail.com"));
+		
 	}
 
 }
--- src/main/java/com/itwill/bookbread/test/MemberServiceTestMain.java
+++ src/main/java/com/itwill/bookbread/test/MemberServiceTestMain.java
@@ -8,16 +8,7 @@ public class MemberServiceTestMain {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		MemberDao memberDao = new MemberDao();
-		//System.out.println(memberDao.create(new Member("bbb","aaa","aaa","aaa","aaa","aaa","aaa","aaa")));
-		//System.out.println(memberDao.update(new Member("heechul", "null", "김희철", "010-6666-6666", "2022-11-11", "서울시 강서구", "kim@testmail.com", "동화")));
-		//System.out.println(memberDao.delete("bbb"));
-		//System.out.println(memberDao.findMember("aaa"));
-		//System.out.println(memberDao.findMember("ddoyoon"));
-		//System.out.println(memberDao.findMember("bbb"));
-		//System.out.println(memberDao.findMemeber());
-		//System.out.println(memberDao.findId("김희철", "010-6666-6666"));
-		//System.out.println(memberDao.findPassword("heechul", "heechul@testmail.com"));
+		
 	}
 
 }
