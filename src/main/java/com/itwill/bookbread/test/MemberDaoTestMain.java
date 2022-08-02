83a649abb3377e2233a99724ad401d724ba0ba13
KimHeeCheul <kim11403@naver.com>
2022-08-02 19:13:42 +0900

    ..

--- src/main/java/com/itwill/bookbread/test/MemberDaoTestMain.java
+++ src/main/java/com/itwill/bookbread/test/MemberDaoTestMain.java
@@ -1,10 +1,11 @@
 package com.itwill.bookbread.test;
 
 import com.itwill.bookbread.dao.MemberDao;
+import com.itwill.bookbread.dto.Member;
 
 public class MemberDaoTestMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
 		MemberDao memberDao = new MemberDao();
--- src/main/java/com/itwill/bookbread/test/MemberServiceTestMain.java
+++ src/main/java/com/itwill/bookbread/test/MemberServiceTestMain.java
@@ -1,13 +1,10 @@
 package com.itwill.bookbread.test;
 
-import com.itwill.bookbread.dao.MemberDao;
-import com.itwill.bookbread.dto.Member;
 
 public class MemberServiceTestMain {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		
 		
 	}
 
