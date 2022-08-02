1b98d5d18e522a8bcf60a4bff0e48988bfb86c08
wkqrl <wkqrl@221.146.149.73>
2022-08-02 23:48:03 +0900

    ..

--- src/main/java/com/itwill/bookbread/test/ProductDaoTestMain.java
+++ src/main/java/com/itwill/bookbread/test/ProductDaoTestMain.java
@@ -1,10 +1,20 @@
 package com.itwill.bookbread.test;
 
+import java.util.List;
+
+import com.itwill.bookbread.dao.ProductDao;
+import com.itwill.bookbread.dto.Product;
+import com.itwill.bookbread.dto.BookType;
+
 public class ProductDaoTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		
+		ProductDao productDao = new ProductDao();
+		
+		System.out.println("전체 도서 리스트");
+		List<Product> productlist = productDao.selectAll();
+		System.out.println(productlist);
 	}
 
 }
