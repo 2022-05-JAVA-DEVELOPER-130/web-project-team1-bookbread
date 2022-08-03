185cfea0098bd96a77533b780b760e48c4c2ca7a
Juniverse <ggolre90@gmail.com>
2022-08-03 17:04:14 +0900

    ..

--- src/main/java/com/itwill/bookbread/service/BookTypeService.java
+++ src/main/java/com/itwill/bookbread/service/BookTypeService.java
@@ -1,5 +1,26 @@
 package com.itwill.bookbread.service;
 
+import java.util.List;
+
+import com.itwill.bookbread.dao.BookTypeDao;
+import com.itwill.bookbread.dto.BookType;
+
 public class BookTypeService {
 
+	private BookTypeDao bookTypeDao;
+	
+	public BookTypeService() throws Exception{
+		bookTypeDao = new BookTypeDao();
+	}
+	
+	// 모든 booktype 출력하기
+	public List<BookType> selectAllBookType() throws Exception {
+		return bookTypeDao.selectAll();
+	}
+	
+	// 번호로 booktype 출력하기
+	public BookType selectBookTypeByNo(int type_no) throws Exception {
+		return bookTypeDao.BooktypeByNo(type_no);
+	}
+	
 }
